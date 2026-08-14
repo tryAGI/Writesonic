0ad2f19d8f84d92d41c8993e086aa737939a11fa
Konstantin S. <havendv@gmail.com>
2026-08-14 19:42:04 +0400

    feat: Updated OpenAPI spec (#34)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Writesonic/Generated/Writesonic.WritesonicClient.g.cs
+++ src/libs/Writesonic/Generated/Writesonic.WritesonicClient.g.cs
@@ -36,10 +36,17 @@ namespace Writesonic
 
         /// <inheritdoc/>
         public global::Writesonic.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Writesonic.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Writesonic.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
